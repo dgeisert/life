9fcb3367a26061358ad735c8f1c9011662bfc1fc
dgeisert <memphis.gatech@gmail.com>
2018-03-31 09:30:39 -0400

    adding amplitude

---
+++ Assets/Scripts/Panel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Panel : MonoBehaviour {
+	public Text title, text;
+	public Button button1, button2;
+	Text button1Text, button2Text;
+
+}
